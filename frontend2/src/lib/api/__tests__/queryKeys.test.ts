b30c9b57353ee5e8b176e4d286ad9f06acdab7b8
Antti <antti@begin.ee>
2026-04-15 15:54:21 +0300

    test(56-02): extend queryKeys tests for loans, borrowers, categories, locations, containers (RED)

--- frontend2/src/lib/api/__tests__/queryKeys.test.ts
+++ frontend2/src/lib/api/__tests__/queryKeys.test.ts
@@ -1,6 +1,11 @@
 import { describe, it, expect } from "vitest";
 import { itemKeys } from "../items";
 import { itemPhotoKeys } from "../itemPhotos";
+import { loanKeys } from "../loans";
+import { borrowerKeys } from "../borrowers";
+import { categoryKeys } from "../categories";
+import { locationKeys } from "../locations";
+import { containerKeys } from "../containers";
 
 describe("itemKeys factory", () => {
   it("all equals ['items']", () => {
@@ -43,5 +48,115 @@ describe("itemPhotoKeys factory", () => {
 
   it("detail(id) equals ['itemPhotos', 'detail', id]", () => {
     expect(itemPhotoKeys.detail("photo-abc")).toEqual(["itemPhotos", "detail", "photo-abc"]);
+  });
+});
+
+describe("loanKeys factory", () => {
+  it("all equals ['loans']", () => {
+    expect(loanKeys.all).toEqual(["loans"]);
+  });
+
+  it("lists() equals ['loans', 'list']", () => {
+    expect(loanKeys.lists()).toEqual(["loans", "list"]);
+  });
+
+  it("list(params) equals ['loans', 'list', params]", () => {
+    expect(loanKeys.list({ active: true })).toEqual(["loans", "list", { active: true }]);
+  });
+
+  it("details() equals ['loans', 'detail']", () => {
+    expect(loanKeys.details()).toEqual(["loans", "detail"]);
+  });
+
+  it("detail(id) equals ['loans', 'detail', id]", () => {
+    expect(loanKeys.detail("loan-1")).toEqual(["loans", "detail", "loan-1"]);
+  });
+});
+
+describe("borrowerKeys factory", () => {
+  it("all equals ['borrowers']", () => {
+    expect(borrowerKeys.all).toEqual(["borrowers"]);
+  });
+
+  it("lists() equals ['borrowers', 'list']", () => {
+    expect(borrowerKeys.lists()).toEqual(["borrowers", "list"]);
+  });
+
+  it("list(params) equals ['borrowers', 'list', params]", () => {
+    expect(borrowerKeys.list({})).toEqual(["borrowers", "list", {}]);
+  });
+
+  it("details() equals ['borrowers', 'detail']", () => {
+    expect(borrowerKeys.details()).toEqual(["borrowers", "detail"]);
+  });
+
+  it("detail(id) equals ['borrowers', 'detail', id]", () => {
+    expect(borrowerKeys.detail("borrower-1")).toEqual(["borrowers", "detail", "borrower-1"]);
+  });
+});
+
+describe("categoryKeys factory", () => {
+  it("all equals ['categories']", () => {
+    expect(categoryKeys.all).toEqual(["categories"]);
+  });
+
+  it("lists() equals ['categories', 'list']", () => {
+    expect(categoryKeys.lists()).toEqual(["categories", "list"]);
+  });
+
+  it("list(params) equals ['categories', 'list', params]", () => {
+    expect(categoryKeys.list({})).toEqual(["categories", "list", {}]);
+  });
+
+  it("details() equals ['categories', 'detail']", () => {
+    expect(categoryKeys.details()).toEqual(["categories", "detail"]);
+  });
+
+  it("detail(id) equals ['categories', 'detail', id]", () => {
+    expect(categoryKeys.detail("cat-1")).toEqual(["categories", "detail", "cat-1"]);
+  });
+});
+
+describe("locationKeys factory", () => {
+  it("all equals ['locations']", () => {
+    expect(locationKeys.all).toEqual(["locations"]);
+  });
+
+  it("lists() equals ['locations', 'list']", () => {
+    expect(locationKeys.lists()).toEqual(["locations", "list"]);
+  });
+
+  it("list(params) equals ['locations', 'list', params]", () => {
+    expect(locationKeys.list({})).toEqual(["locations", "list", {}]);
+  });
+
+  it("details() equals ['locations', 'detail']", () => {
+    expect(locationKeys.details()).toEqual(["locations", "detail"]);
+  });
+
+  it("detail(id) equals ['locations', 'detail', id]", () => {
+    expect(locationKeys.detail("loc-1")).toEqual(["locations", "detail", "loc-1"]);
+  });
+});
+
+describe("containerKeys factory", () => {
+  it("all equals ['containers']", () => {
+    expect(containerKeys.all).toEqual(["containers"]);
+  });
+
+  it("lists() equals ['containers', 'list']", () => {
+    expect(containerKeys.lists()).toEqual(["containers", "list"]);
+  });
+
+  it("list(params) equals ['containers', 'list', params]", () => {
+    expect(containerKeys.list({})).toEqual(["containers", "list", {}]);
+  });
+
+  it("details() equals ['containers', 'detail']", () => {
+    expect(containerKeys.details()).toEqual(["containers", "detail"]);
+  });
+
+  it("detail(id) equals ['containers', 'detail', id]", () => {
+    expect(containerKeys.detail("cont-1")).toEqual(["containers", "detail", "cont-1"]);
   });
 });
